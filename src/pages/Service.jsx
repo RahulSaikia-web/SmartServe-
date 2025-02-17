5d0ed7b08ecf5028b34c16259f0eeb109192e0cf
RahulSaikia-web <rahulsaikia682@gmail.com>
2025-02-17 20:57:39 +0530

    New and noteworthy Service page frontend complete

--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -1,11 +1,65 @@
-import React from 'react'
-
+import React from "react";
+import "./Service.css";
 function Service() {
   return (
     <>
-      <h1>New and noteworthy</h1>
+      <div className="container">
+        <div className="service-heading">
+          <h1>New and noteworthy</h1>
+        </div>
+        <div className="services">
+          <div className="service-box">
+            <div className="service-top">
+              <img
+                src="https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template/w_233,dpr_1,fl_progressive:steep,q_auto:low,f_auto,c_limit/images/growth/home-screen/1681371120474-301f47.jpeg"
+                alt=""
+              />
+            </div>
+            <div className="service-bottom">
+              <h2>Bathroom & Kitchen Cleaning</h2>
+            </div>
+          </div>
+
+          {/* Repeat Demo Service Box */}
+          <div className="service-box">
+            <div className="service-top">
+              <img
+                src="https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template/w_233,dpr_1,fl_progressive:steep,q_auto:low,f_auto,c_limit/images/growth/home-screen/1681371120474-301f47.jpeg"
+                alt=""
+              />
+            </div>
+            <div className="service-bottom">
+              <h2>Bathroom & Kitchen Cleaning</h2>
+            </div>
+          </div>
+          <div className="service-box">
+            <div className="service-top">
+              <img
+                src="https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template/w_233,dpr_1,fl_progressive:steep,q_auto:low,f_auto,c_limit/images/growth/home-screen/1681371120474-301f47.jpeg"
+                alt=""
+              />
+            </div>
+            <div className="service-bottom">
+              <h2>Bathroom & Kitchen Cleaning</h2>
+            </div>
+          </div>
+          <div className="service-box">
+            <div className="service-top">
+              <img
+                src="https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template/w_233,dpr_1,fl_progressive:steep,q_auto:low,f_auto,c_limit/images/growth/home-screen/1681371120474-301f47.jpeg"
+                alt=""
+              />
+            </div>
+            <div className="service-bottom">
+              <h2>Bathroom & Kitchen Cleaning</h2>
+            </div>
+          </div>
+
+          {/* Demo Box End Here  */}
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Service
+export default Service;
